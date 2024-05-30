7529931566d1f1476dbefbeedde759bc228241a7
James Morgan <157502032+JamesVTT@users.noreply.github.com>
2024-05-30 13:17:28 -0600

    Add human logic

--- javascript.js
+++ javascript.js
@@ -6,3 +6,12 @@ function getComputerChoice() {
     let computerChoice = Math.floor(Math.random()*3);
     console.log(mainArray[computerChoice]);
 }
+
+function getHumanChoice () {
+    let humanChoice = prompt('Rock, Paper, or Scissors, tiny human?','');
+    if (humanChoice === 'Rock' || humanChoice === 'rock' ||humanChoice === 'Paper' ||humanChoice === 'paper' ||humanChoice === 'Scissors' ||humanChoice === 'scissors') {
+        console.log(humanChoice)
+    } else {
+        console.log('That wasn\'t a selection now was is it? Try again.')
+    }
+}
