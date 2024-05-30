11cd1e342d47ace8e2ea079903c342ac5d648aae
James Morgan <157502032+JamesVTT@users.noreply.github.com>
2024-05-30 13:35:59 -0600

    Made humanChoice case insensitive

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 console.log("Hello World! Ready to play?");
 
 let mainArray = ['Rock', 'Paper', 'Scissors'];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random()*3);
@@ -8,8 +10,8 @@ function getComputerChoice() {
 }
 
 function getHumanChoice () {
-    let humanChoice = prompt('Rock, Paper, or Scissors, tiny human?','');
-    if (humanChoice === 'Rock' || humanChoice === 'rock' ||humanChoice === 'Paper' ||humanChoice === 'paper' ||humanChoice === 'Scissors' ||humanChoice === 'scissors') {
+    let humanChoice = prompt('Rock, Paper, or Scissors, tiny human?','').toLowerCase();
+    if (humanChoice === 'rock' ||humanChoice === 'paper' ||humanChoice === 'scissors') {
         console.log(humanChoice)
     } else {
         console.log('That wasn\'t a selection now was is it? Try again.')
