fc073821df211bec7bcec779f34345cbaa1be161
James Morgan <157502032+JamesVTT@users.noreply.github.com>
2024-06-03 14:35:53 -0600

    Add prelim structure for playRound

--- javascript.js
+++ javascript.js
@@ -1,19 +1,35 @@
 console.log("Hello World! Ready to play?");
 
-let mainArray = ['Rock', 'Paper', 'Scissors'];
+let mainArray = ['rock', 'paper', 'scissors'];
 let humanScore = 0;
 let computerScore = 0;
 
+
+// Function to have computer make a random selection
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random()*3);
-    console.log(mainArray[computerChoice]);
+    return mainArray[Math.floor(Math.random()*3)];
 }
 
+// Function to allow user to input their selection 
 function getHumanChoice () {
-    let humanChoice = prompt('Rock, Paper, or Scissors, tiny human?','').toLowerCase();
-    if (humanChoice === 'rock' ||humanChoice === 'paper' ||humanChoice === 'scissors') {
-        console.log(humanChoice)
+    let input = prompt('Rock, Paper, or Scissors, tiny human?','').toLowerCase();
+    if (input === 'rock' ||input === 'paper' ||input === 'scissors') {
+        return input
     } else {
         console.log('That wasn\'t a selection now was is it? Try again.')
     }
 }
+
+// Function to play one round
+function playRound(humanChoice, computerChoice) {
+    console.log (humanChoice + 'This is working' + computerChoice);
+
+}
+
+
+ 
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  playRound(humanSelection, computerSelection);
