ae447ea6da41bc02d2900c796e865a87b9607171
James Morgan <157502032+JamesVTT@users.noreply.github.com>
2024-06-03 16:29:59 -0600

    Finalize Round Logic and add playGame function

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
-console.log("Hello World! Ready to play?");
+console.log("Hello World! Ready to play?")
+console.log('You will have 5 rounds. The one with the most points wins');
+console.log("To play type 'playGame();' and press Enter ");
 
 let mainArray = ['rock', 'paper', 'scissors'];
 let humanScore = 0;
 let computerScore = 0;
-
+let round = 0;
 
 // Function to have computer make a random selection
 function getComputerChoice() {
@@ -22,14 +24,51 @@ function getHumanChoice () {
 
 // Function to play one round
 function playRound(humanChoice, computerChoice) {
-    console.log (humanChoice + 'This is working' + computerChoice);
-
+    if (humanChoice !== undefined) {
+        if (humanChoice === computerChoice) {
+            console.log ('Draw')
+        } else if (humanChoice !== computerChoice) {
+            if (humanChoice == 'rock' && computerChoice == 'paper') {
+                console.log('You Lose! Your rock has been covered up by the paper, bummer.')
+                computerScore++; 
+            } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
+                console.log('You Win! Smashing success!')
+                humanScore++;
+            } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+                console.log('You Lose! Your paper has been cut to pieces by the scissors, bummer.')
+                computerScore++;
+            } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+                console.log('You Win! This is something to write home about!')
+                humanScore++;
+            } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+                console.log('You Lose! Your scissors have been pulverized by the rock, bummer.')
+                computerScore++;
+            }else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+                console.log('You Win! You cut your way through the competition.')
+                humanScore++;
+            }
+        }
+    }  
+console.log('You have ' + humanScore + ' points');
+console.log('The computer has ' + computerScore + ' points');
+console.log('');
+    if (round == 4) {
+        if (humanScore > computerScore) {
+            console.log('Congratulations you win! You beat the computer by' (humanScore-computerScore))
+        } else {
+            console.log('Welp you lost by ' + (computerScore-humanScore));
+        }
+        console.log('Game Over!')
+    }
 }
 
-
- 
-  
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-  
-  playRound(humanSelection, computerSelection);
+//function to play a game of 5 rounds. 
+function playgame() {
+      
+    for (round = 0; round < 5; round++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    return;
+}
